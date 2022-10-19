5d80705cb1fd25d11852eb3249b426f09fdc0f91
kskotenko <kskotenko@gmail.com>
2022-10-19 14:47:44 +0300

    initiate dictionary app project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.png'
+import './App.css'
+
+function App() {
+	return (
+		<div className="App container-sm">
+			<header className="App-header">
+				<img src={logo} className="App-logo img-fluid" alt="logo" />
+				<h1>Any word you are concerned with? 🤔</h1>
+				<h2>Let's figure it out!</h2>
+			</header>
+		</div>
+	)
+}
+
+export default App
